f1f93d8fed281cc90d2be766c83d13cd6b7f92fa
joseroberto <joseroberto@alunos.utfpr.edu.br>
2022-06-28 09:30:08 -0300

    1

--- index.js
+++ index.js
@@ -82,7 +82,10 @@ app.post('/authenticate', async (req, res) => {
 
     } catch (error) {
         if (error) {
+            alert('senha');
             return res.redirect('/?=SenhaIncorreta')
+
+
 
         }
     }
