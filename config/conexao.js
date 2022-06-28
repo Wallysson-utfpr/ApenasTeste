e68e1e115909c05691510a9ed75458bf6d7d1348
joseroberto <joseroberto@alunos.utfpr.edu.br>
2022-06-28 13:34:31 -0300

    1

--- config/conexao.js
+++ config/conexao.js
@@ -3,9 +3,12 @@
 
 const mongoose = require('mongoose');
 const uri = "mongodb://localhost:27017/dadosapi";
+
+// url banco de dados online
 const url_prod ="mongodb+srv://Wallysson:Valdemir0123456@cluster0.bmdvm.mongodb.net/dadosapi"
 
 mongoose.connect(url_prod, { useNewUrlParser: true, useUnifiedTopology: true });
+
 module.exports = mongoose;
 
  
